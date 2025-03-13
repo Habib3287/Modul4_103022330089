c57a0c765e458cf55c7022a0c79949dea41d8a87
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-13 09:59:38 +0700

    Menambahkan class KodeProduk

--- Program.cs
+++ Program.cs
@@ -1,3 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hi");
+
+public class KodeProduk
+{
+    public String kodeProduk { get; set; }
+    public String produkElektronik { get; set; }
+};
+
+class MainClass
+{
+    static void Main()
+    {
+        List<KodeProduk> lProduk = new List<KodeProduk>(
+            new KodeProduk { produkElektronik = "Laptop", kodeProduk = "E100"},
+            new KodeProduk { produkElektronik = "Smartphone", kodeProduk = "E101"},
+            new KodeProduk { produkElektronik = "Tablet", kodeProduk = "E102"},
+            new KodeProduk { produkElektronik = "Headset", kodeProduk = "E103"},
+            new KodeProduk { produkElektronik = "Keyboard", kodeProduk = "E104"},
+            new KodeProduk { produkElektronik = "Mousr", kodeProduk = "E105"},
+            new KodeProduk { produkElektronik = "Printer", kodeProduk = "E106"},
+            new KodeProduk { produkElektronik = "Monitor", kodeProduk = "E107"},
+            new KodeProduk { produkElektronik = "Smartwatch", kodeProduk = "E108"}
+            new KodeProduk { produkElektronik = "Kamera", kodeProduk = "E109"}
+
+            );
+    }
+}
+
+
+
+
+
